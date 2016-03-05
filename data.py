3ebb333fcf7cbb7ee71278661af5a9707157a495
M. Woellhaf <woellhaf@mail.tu-berlin.de>
2016-03-05 11:00:25 +0100

    Added data generator script.

---
+++ data.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from data.generator import Generator
+
+N_CLASSES = 3
+N_SAMPLES = 1000
+
+g = Generator(2, N_CLASSES)
+g.generate(N_SAMPLES)
+g.save('data_c_3_n_1000')
+
+fig = plt.figure()
+plt.scatter(g.X[:, 0], g.X[:, 1], c=g.Y, s=50)
+plt.show()
